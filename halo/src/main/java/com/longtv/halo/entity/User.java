faa14e8a780781f9c0bd4e0f69f1bb746f8f0159
longminions <longtranvu047@gmail.com>
2025-04-27 23:20:14 +0700

    #5: complete entity user

--- halo/src/main/java/com/longtv/halo/entity/User.java
+++ halo/src/main/java/com/longtv/halo/entity/User.java
@@ -25,7 +25,4 @@ public class User {
 
 	@Column(name = "EMAIL")
 	private String email;
-
-	public User orElseThrow(Object o) {
-	}
 }
--- halo/src/main/java/com/longtv/halo/service/UserService.java
+++ halo/src/main/java/com/longtv/halo/service/UserService.java
@@ -2,15 +2,10 @@ package com.longtv.halo.service;
 
 import com.longtv.halo.dto.UserBean;
 import com.longtv.halo.entity.User;
-import com.longtv.halo.handle.UserNotFoundException;
 import com.longtv.halo.repository.UserRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.util.CollectionUtils;
-
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -41,10 +36,15 @@ public class UserService {
         return userBeansList;
     }
 
-    public UserBean updateUser(Long id, UserBean userBean) {
-        User user = userRepository.findOneUsersById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));;
-        if(user == null) {
+    public void updateUser(Long id, UserBean userBean) {
+        User user = userRepository.findOneUsersById(id);
 
+        if (user == null) {
+            user.setEmail(user.getEmail());
+            user.setLastName(userBean.getLastName());
+            user.setFirstName(userBean.getFirstName());
         }
+
+        userRepository.save(user);
     }
 }
