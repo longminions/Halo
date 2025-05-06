0f50c46fa466e76b750af8723a9763d7341eb721
DESKTOP-02246KD\longtv <longtv@oneempower.com>
2025-05-06 17:50:18 +0700

    #7: add feature eating fish by aduirno r3

---
+++ halo/src/main/java/com/longtv/halo/controller/AquariumController.java
@@ -0,0 +1,40 @@
+package com.longtv.halo.controller;
+
+import com.longtv.halo.entity.*;
+import com.longtv.halo.service.*;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/aqua/action")
+public class AquariumController {
+	
+	private String deviceState = "OFF";
+	
+	@Autowired
+	private AquariumAction aquariumAction;
+	
+	@PostMapping("/eat")
+	private HttpStatus doEat(@RequestBody FishActionRequest fishActionReq ) {
+		aquariumAction.doEats(fishActionReq);
+		return HttpStatus.OK ;
+	}
+
+	@GetMapping("/control")
+	public String controlDevice() {
+		return deviceState;
+	}
+	
+	@GetMapping("/turnOn")
+	public String turnOn() {
+		deviceState = "ON";
+		return "Device is ON";
+	}
+	
+	@GetMapping("/turnOff")
+	public String turnOff() {
+		deviceState = "OFF";
+		return "Device is OFF";
+	}
+}
---
+++ halo/src/main/java/com/longtv/halo/entity/FishActionRequest.java
@@ -0,0 +1,4 @@
+package com.longtv.halo.entity;
+
+public class FishActionRequest {
+}
